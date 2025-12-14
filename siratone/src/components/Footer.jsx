d348cb5c11a500d3b41c43c306aaccdb86fec743
eleniss <elena.fernandez.leon@estudiantat.upc.edu>
2025-12-14 19:07:54 +0100

    footer switch animation

--- siratone/src/components/Footer.jsx
+++ siratone/src/components/Footer.jsx
@@ -1,17 +1,24 @@
 import './Footer.css';
-import { Link } from 'react-router-dom'; //------------
-  
+import { Link, useLocation } from 'react-router-dom'; 
+
 function Footer() {
+    const location = useLocation();
+    const currentPath = location.pathname;
+    const isFavoritesActive = currentPath === '/FavoritesPage';
+    
     return (
       <div className="footer">
-        <Link to="/" className="footer-button"> {/* Link para Home */}
+        <div 
+        className={`active-indicator ${isFavoritesActive ? 'is-active-favorites' : 'is-active-home'}`}
+        ></div>
+        <Link to="/" className="footer-button"> 
           <img src="/icons/home.svg" alt="Home" />
         </Link>
-        <Link to="/FavoritesPage" className="footer-button"> {/* Link para Favorits */}
+        <Link to="/FavoritesPage" className="footer-button"> 
           <img src="/icons/save.svg" alt="Guardados" />
         </Link>
       </div>
-    );
+      );
 }
 
 export default Footer;
